4f28856a1397f7f485ad5011a437ccce19bfbd30
Renan Rhoden <renanrhoden@gmail.com>
2017-10-02 14:07:35 -0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/FPI_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FPI_1.dir/main.cpp.o"
+  "FPI_1.pdb"
+  "FPI_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FPI_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <jpeglib.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
